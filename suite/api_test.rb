8bcedab3c368b3259b0779c9775a1c8cc1290596
Carson Jardine <carsonjardine@gmail.com>
2021-05-19 20:55:51 -0600

    DRY up tests and add further testing

--- suite/api_test.rb
+++ suite/api_test.rb
@@ -4,19 +4,21 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class ApiTest < Minitest::Test
+  def setup
+    @url = 'http://www.omdbapi.com/'
+    @apikey = 'apikey=8d48d285'
+  end
 
   def test_no_api_key
-    skip
-    make_request('?s=star', 'http://www.omdbapi.com/')
-    # puts last_response
+    make_request('?s=star', "#{@url}")
+    puts last_response
     assert_equal 'False', last_response[:Response]
     assert_equal 'No API key provided.', last_response[:Error]
   end
 
   def test_thomas_search_has_valid_returns
-    skip
-  # TODO: TAKE OUT API KEY!
-    make_request('?s=thomas&apikey=8d48d285', 'http://www.omdbapi.com/')
+    make_request("?s=thomas&#{@apikey}", "#{@url}")
+
     last_response[:Search].each do |res|
       # Verify all titles are a relevant match
       assert_includes res[:Title].downcase, 'thomas'
@@ -25,71 +27,98 @@ class ApiTest < Minitest::Test
       assert_equal [:Title, :Year, :imdbID, :Type, :Poster], res.keys
 
       # Verify values are all of the correct object class
-      assert_instance_of String, res[:Title]
-      assert_instance_of String, res[:Year]
-      assert_instance_of String, res[:imdbID]
-      assert_instance_of String, res[:Type]
-      assert_instance_of String, res[:Poster]
+      res.keys.each do |key|
+        assert_instance_of String, res[key]
+      end
 
       # Verify year matches correct format
-      # TODO: come back and fix this
-      assert res[:Year].length >= 4
+      assert valid_year?(res)
     end
   end
 
-  def test_each_title_is_accessible_via_imdbID
-    skip
-    make_request('?s=thomas&page=1&apikey=8d48d285', 'http://www.omdbapi.com/')
-    assert_equal 10, last_response[:Search].count
-    one = last_response[:Search][0]
-    two = last_response[:Search][1]
-    three = last_response[:Search][2]
-    four = last_response[:Search][3]
-    five = last_response[:Search][4]
-    six = last_response[:Search][5]
-    seven = last_response[:Search][6]
-    eight = last_response[:Search][7]
-    nine = last_response[:Search][8]
-    ten = last_response[:Search][9]
+  def test_each_pg1_title_is_accessible_via_imdbID
+    make_request("?s=thomas&#{@apikey}", "#{@url}")
 
-    make_request("?i=#{one[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal one[:imdbID], last_response[:imdbID]
+    results = last_response[:Search]
 
-    make_request("?i=#{two[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal two[:imdbID], last_response[:imdbID]
-
-    make_request("?i=#{three[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal three[:imdbID], last_response[:imdbID]
-
-    make_request("?i=#{four[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal four[:imdbID], last_response[:imdbID]
-
-    make_request("?i=#{five[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal five[:imdbID], last_response[:imdbID]
-
-    make_request("?i=#{six[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal six[:imdbID], last_response[:imdbID]
-
-    make_request("?i=#{seven[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal seven[:imdbID], last_response[:imdbID]
-
-    make_request("?i=#{eight[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal eight[:imdbID], last_response[:imdbID]
-
-    make_request("?i=#{nine[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal nine[:imdbID], last_response[:imdbID]
-
-    make_request("?i=#{ten[:imdbID]}&apikey=8d48d285", 'http://www.omdbapi.com/')
-    assert_equal ten[:imdbID], last_response[:imdbID]
+    results.each do |res|
+      make_request("?i=#{res[:imdbID]}&#{@apikey}", "#{@url}")
+      assert_equal res[:Title], last_response[:Title]
+    end
   end
 
-  def test_all_poster_links_are_working
-    make_request('?s=thomas&page=1&apikey=8d48d285', 'http://www.omdbapi.com/')
-    assert_equal 10, last_response[:Search].count
+  def test_all_pg1_poster_links_are_working
+    make_request("?s=thomas&#{@apikey}", "#{@url}")
     results = last_response[:Search]
 
     results.each do |res|
       assert valid_uri?(res[:Poster])
     end
+  end
+
+  def test_for_duplicates_on_first_five_pgs
+    results = []
+    index = 1
+
+    5.times do
+      make_request("?s=thomas&page=#{index}&#{@apikey}", "#{@url}")
+      results << last_response[:Search]
+      index +=1
+    end
+
+    results = results.flatten
+
+    unique_results = results.uniq {|res| res[:imdbID]}
+
+    assert_equal 50, results.count
+    assert_equal 49, unique_results.count 
+    refute_equal results.count, unique_results.count
+  end
+
+  def test_it_can_return_movies_w_keyword_carson_year_2020
+    # testing all movies with keyword 'carson' that were released in 2020
+    make_request("?s=carson&type=movie&y=2020&#{@apikey}", "#{@url}")
+
+    assert_equal '5', last_response[:totalResults]
+
+    last_response[:Search].each do |res|
+      assert_equal 'movie', res[:Type]
+      refute_equal 'series', res[:Type]
+
+      assert_equal '2020', res[:Year]
+
+      assert_includes res[:Title].downcase, 'carson'
+    end
+  end
+
+  def test_valid_year
+    good_result = {Year: "2020–"}
+    assert valid_year?(good_result)
+
+    # used assert_equal false below instead of `refute valid_year?(bad_result)` because I wanted to make sure we weren't getting a nil response
+    bad_result = {Year: "sfgsr"}
+    assert_equal false, valid_year?(bad_result)
+
+    bad_result = {Year: "35435"}
+    assert_equal false, valid_year?(bad_result)
+  end
+
+  def test_year_checker
+    good_result = "2020"
+    assert year_checker(good_result)
+
+    bad_result = "50"
+    assert_equal false, year_checker(bad_result)
+  end
+
+  def test_year_splitter
+    year = "1999–2008"
+    expected = %w(1 9 9 9 2 0 0 8)
+
+    assert_equal expected, year_splitter(year)
+
+    year = "2017–"
+    expected = %w(2 0 1 7)
+    assert_equal expected, year_splitter(year)
   end
 end
